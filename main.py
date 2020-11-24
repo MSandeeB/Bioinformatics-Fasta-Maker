95e9af4b722ba19c3c8b31287ba4c593639d6f96
MSandeeB <57465848+MSandeeB@users.noreply.github.com>
2020-11-24 17:24:01 -0500

    moved comments above

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import random
-import io #import packages
+#import packages
+import io 
 def dnagenerator():
-  nucleotideslist = list("AGCT") #makes individual letters into seperate strings
+  #makes individual letters into seperate strings
+  nucleotideslist = list("AGCT") 
   subrepeata = ''
-  randomnum1 = random.randint(1,150) #randomly chooses a number from 1-150
+  #randomly chooses a number from 1-150
+  randomnum1 = random.randint(1,150) 
   for i in range(randomnum1):
-    subrepeata += random.choice(nucleotideslist) #adds a random sequence of nucleotides amounting to random1 to subrepeata
+    #adds a random sequence of nucleotides amounting to random1 to subrepeata
+    subrepeata += random.choice(nucleotideslist) 
   #print(subrepeata)
 
   subrepeatb = ''
-  randomnum2 = 150 - randomnum1 #subtracts number of nucleotides in subrepeata from amount of subrepeatb
+  #subtracts number of nucleotides in subrepeata from amount of subrepeatb
+  randomnum2 = 150 - randomnum1 
   for i in range(randomnum2):
-      subrepeatb += random.choice(nucleotideslist) #adds a random sequence of nucleotides amounting to random2 to subrepeatb
+      #adds a random sequence of nucleotides amounting to random2 to subrepeatb
+      subrepeatb += random.choice(nucleotideslist) 
   #print(subrepeatb)
   subset1 = subrepeata + subrepeatb
-  return(subset1 * 2) #prints full sequence
-generator = dnagenerator() #assigns function to a variable
+  #prints full sequence
+  return(subset1 * 2)
+#assigns function to a variable
+generator = dnagenerator() 
 
 filename = input("File name is: ")
 header = input("File header: ")
 #user inputs
-def spidroinFile(filename, header): #function to create fasta file
-  try: #not neededbut helps make the code run smoothly even with errors
+#function to create fasta file
+def spidroinFile(filename, header):
+  #try is not needed but helps make the code run smoothly even with errors
+  try: 
     with open(filename + ".fasta", "w") as fasta_handle: 
-      fasta_handle.write(">Gene info " + header + "\n") #adds header into the file
-      fasta_handle.write(generator) #adds the generated DNA sequence
-  except IOError as err: #detects error in the function
+      #adds header into the file
+      fasta_handle.write(">Gene info " + header + "\n")
+      #adds the generated DNA sequence
+      fasta_handle.write(generator)
+  #detects error in the function
+  except IOError as err: 
     print("error")
   return fasta_handle
 
